d779d3e42e7b1ea7c9d61a51d9f716be275da789
yian <yiankonlie@gmail.com>
2023-03-23 16:18:48 +0800

    String

--- chapter10_thread_teacher/src/com/atguigu01/create/runnable/EvenNumberTest.java
+++ chapter10_thread_teacher/src/com/atguigu01/create/runnable/EvenNumberTest.java
@@ -4,16 +4,12 @@ package com.atguigu01.create.runnable;
  * ClassName: EvenNumberTest
  * Description:
  *
- *
- *
- *
- *
  * @Author 尚硅谷-宋红康
  * @Create 11:10
  * @Version 1.0
  */
 //① 创建一个实现Runnable接口的类
-    class EvenNumberPrint1 implements Runnable{
+class EvenNumberPrint1 implements Runnable {
     @Override
     public void run() {
 
@@ -23,18 +19,20 @@ package com.atguigu01.create.runnable;
         }
     }
 }
-class EvenNumberPrint implements Runnable{
-//    private double money = 1000;
+
+class EvenNumberPrint implements Runnable {
+    //    private double money = 1000;
     //② 实现接口中的run() -->将此线程要执行的操作，声明在此方法体中
     @Override
     public void run() {
 
-        for(int i = 1;i <= 100;i++){
+        for (int i = 1; i <= 100; i++) {
             System.out.println(Thread.currentThread().getName() + ":" + i);
         }
 
     }
 }
+
 public class EvenNumberTest {
     public static void main(String[] args) {
         //③ 创建当前实现类的对象
@@ -43,22 +41,23 @@ public class EvenNumberTest {
         Thread t1 = new Thread(p);
         //⑤ Thread类的实例调用start():1.启动线程 2.调用当前线程的run()
         t1.start();
-        EvenNumberPrint1 p1=new EvenNumberPrint1();
-        Thread t2=new Thread(p1);
+        EvenNumberPrint1 p1 = new EvenNumberPrint1();
+        Thread t2 = new Thread(p1);
         t2.start();
 
 
         //main()方法对应的主线程执行的操作：
-        for(int i = 1;i <= 100;i++){
+        for (int i = 1; i <= 100; i++) {
             System.out.println(Thread.currentThread().getName() + ":" + i);
         }
 
         /*
-        * 拓展：再创建一个线程，用于遍历100以内的偶数
-        *
-        * */
+         * 拓展：再创建一个线程，用于遍历100以内的偶数
+         *
+         * */
         Thread t3 = new Thread(p);
         t3.start();
-        System.out.println(Thread.currentThread().getName() );
-        System.out.println(Thread.getName() );
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName());
     }
+}
